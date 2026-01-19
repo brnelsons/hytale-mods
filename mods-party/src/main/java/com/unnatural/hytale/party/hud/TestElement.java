ddca5787ca36e31d42834db1ec0b94e06b46263b
Brian Nelson <brnelsons139@gmail.com>
2026-01-19 01:28:53 -0600

    updated test ui element

--- mods-party/src/main/java/com/unnatural/hytale/party/hud/TestElement.java
+++ mods-party/src/main/java/com/unnatural/hytale/party/hud/TestElement.java
@@ -4,8 +4,12 @@ import com.unnatural.codegen.CustomTemplate;
 
 @CustomTemplate(
         target = "example.ui",
+        // language=UI
         value = """
-                TestTestTest
+                Group #MyGroup {
+                    Anchor: (Width: 400, Height: 250);
+                    Backgrounmd: #1a1a2e(0.95);
+                }
                 """
 )
 public class TestElement {
